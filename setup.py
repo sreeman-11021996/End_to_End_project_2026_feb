5549664b401cb7cf0e59c17fe3f35e03fa7c97d1
Sreeman <sreemanbitsmech@gmail.com>
2026-02-27 17:59:06 +0530

    setup

--- setup.py
+++ setup.py
@@ -1,14 +1,29 @@
 from setuptools import find_packages, setup
 from typing import List
 
-def get_requirements(file_path:str)->List[str]:
-    pass
+# Constants
+HYPEN_E_DOT = "-e ."
+REQUIREMENTS_FILE_PATH = "requirements.txt"
 
+
+def get_requirements(file_path:str)->List[str]:
+    requirements = []
+    
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n","") for req in requirements]
+        
+    if HYPEN_E_DOT in requirements:
+        requirements.remove(HYPEN_E_DOT)        
+        
+    return requirements
+
+        
 setup(
     name = "end-to-end-ml-project",
     author= "sreeman",
     author_email= "sreemanbitsmech@gmail.com",
     packages= find_packages(),
-    install_requires= get_requirements("requirements.txt")
+    install_requires= get_requirements(REQUIREMENTS_FILE_PATH)
 )
 
